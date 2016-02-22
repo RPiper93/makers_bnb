c03195dfb3dc796447b5e84bb3bfca1c9540253b
Rebecca Piper <piper.rebecca93@gmail.com>
2016-02-22 15:43:24 +0000

    Created /spaces and /spaces/new paths. Created spaces.erb and spaces_new.erb. Wrote feature tests.

--- app/app.rb
+++ app/app.rb
@@ -3,7 +3,13 @@ ENV['RACK_ENV'] ||= 'development'
 require 'sinatra/base'
 
 class MakersBnb < Sinatra::Base
+  get '/spaces' do
+    erb :spaces
+  end
 
+  get '/spaces/new' do
+    erb :spaces_new
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,7 @@ Capybara.app = MakersBnb
 
 RSpec.configure do |config|
   config.include Capybara::DSL
-  
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
@@ -22,16 +22,16 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
+  #config.before(:suite) do
+  #  DatabaseCleaner.strategy = :transaction
+  #   DatabaseCleaner.clean_with(:truncation)
+  # end
 
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
+  # config.before(:each) do
+  #   DatabaseCleaner.start
+  # end
 
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
+  # config.after(:each) do
+  #   DatabaseCleaner.clean
+  # end
 end
