f6c677c1d432bf5560a742ffb343e1c26d2c364d
Tom Pickard <tcpickard94@gmail.com>
2016-02-22 15:19:14 +0000

    WIP - started user story 2
    Created sign-up form on index page
    Created index route
    Written first feature test
    Disabled database cleaner for time being

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,12 @@
 ENV['RACK_ENV'] ||= 'development'
 
+require './app/database_mapper_helper.rb'
 require 'sinatra/base'
 
 class MakersBnb < Sinatra::Base
-
+  get '/' do
+    erb :index
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
