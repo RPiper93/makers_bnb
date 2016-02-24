85930ec80b224931c8e871e501fd9ced409db5af
Rebecca Piper <piper.rebecca93@gmail.com>
2016-02-24 13:59:06 +0000

    WIP: Wrote feature test for User Story 12

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -20,19 +20,27 @@ def create_user(first_name: "Sam",
                 password_confirmation: "123abc"
                )
   visit('/')
-  fill_in :first_name, with: first_name 
-  fill_in :last_name, with: last_name 
-  fill_in :email, with: email 
-  fill_in :password, with: password 
-  fill_in :password_confirmation, with: password_confirmation 
+  fill_in :first_name, with: first_name
+  fill_in :last_name, with: last_name
+  fill_in :email, with: email
+  fill_in :password, with: password
+  fill_in :password_confirmation, with: password_confirmation
   click_button "Sign-up"
 end
 
 def sign_in
-  User.create(first_name: "Sam", last_name: "Morgan", 
+  User.create(first_name: "Sam", last_name: "Morgan",
   email: "sam@email.com", password: "Abc123", password_confirmation: "Abc123")
   visit '/user/sign_in'
   fill_in :email, with: "sam@email.com"
   fill_in(:password, with: "Abc123")
   click_button "Submit"
+end
+
+def make_request
+    create_user(first_name: "Joe",last_name: "Schmo", email: "joe@email.com" )
+    click_link "Sam's space"
+    fill_in :start_date, with: Date.new(2016,02,23)
+    fill_in :end_date, with: Date.new(2016, 02, 24)
+    click_button "Request a booking"
 end
