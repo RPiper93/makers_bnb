0088691c4db22a92ccfce7ade3746de2a3d97eac
Tom Pickard <tcpickard94@gmail.com>
2016-02-24 09:59:27 +0000

    Refactored logic into seperate controllers

---
+++ app/controllers/users.rb
@@ -0,0 +1,39 @@
+class MakersBnb < Sinatra::Base
+  delete '/' do 
+    session[:user] = nil
+    redirect ('/')
+  end 
+
+  post '/user/new' do
+    user = User.create(first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation])
+    if user.saved? 
+      session[:user] = user.id
+      redirect('/spaces')
+    else 
+      flash.next[:errors] = user.errors.full_messages
+      redirect('/')
+    end
+  end
+
+  get '/user/sign_in' do 
+    erb :sign_in
+  end
+
+  post '/user/sign_in' do 
+    user = User.authenticate(params[:email],params[:password])
+    if user == :nonexistent_user
+      flash.next[:errors] = ["User does not exist"]
+      redirect('/user/sign_in')
+    elsif user == :incorrect_password 
+      flash.next[:errors] = ["Incorrect password"]
+      redirect('/user/sign_in')
+    else 
+      session[:user] = user.id
+      redirect('/spaces')
+    end
+  end
+end
