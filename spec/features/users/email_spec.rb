763a53e5f6447bc27770c34733e0fc770f026fc2
Alain Lemaire <jaxdid@gmail.com>
2016-02-28 22:23:25 +0100

    Added feature test for email confirmation upon listing a space.

--- spec/features/users/email_spec.rb
+++ spec/features/users/email_spec.rb
@@ -3,4 +3,10 @@ feature 'Email notification' do
     create_user
     expect(page).to have_content('Sign-up confirmation email sent')    
   end
+
+  scenario 'sent upon successfully listing a space' do
+    create_user
+    create_space
+    expect(page).to have_content('Space listing confirmation email sent')
+  end
 end
